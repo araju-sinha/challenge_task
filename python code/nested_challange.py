312ba126145f335c4add2e0c4b718d809bde04b1
Aarju Sinha <49508889+araju-sinha@users.noreply.github.com>
2023-04-27 14:16:21 +0530

    Update nested_challange.py

--- python code/nested_challange.py
+++ python code/nested_challange.py
@@ -1,26 +1,23 @@
-def getKey(obj: dict):
-    keys = list(obj)
-    if len(keys) != 1:
-        raise Exception('either multiple keys or empty dict found')
-    else:
-        return keys[0]
-
-
-def getNestedValue(obj: dict, key: str, isFound = False):
-    # print(obj, key, isFound)
-    if type(obj) is not dict and not isFound:
-        return None
-    if (isFound or (key in obj.keys())) :
-        if type(obj[key]) is dict:
-            return getNestedValue(obj[key], getKey(obj[key]), True)
+def key_values(object_dict, key):
+    keys = key.split("/")
+#    print("keys after split", keys)
+    for k in keys:
+#        print("k value to pick", k)
+# Applying and to check
+        if isinstance(object_dict, dict) and k in object_dict:
+            object_dict = object_dict[k]
+#            print("object_dict value ", object_dict)
         else:
-            # print(f'obj[getKey(obj)]: {obj[getKey(obj)]}')
-            return obj[getKey(obj)]
-    else:
-        nestedKey = getKey(obj)
-        return getNestedValue(obj[nestedKey], key, False)
+            return None
+    
+    return object_dict
 
-if __name__ == '__main__':
-    obj = {'a': {'b': {'c': 'd'}}}
-    value = getNestedValue(obj, 'c')
-    print(value)
+object1 = {"a": {"b": {"c": "d"}}}
+key1 = "a/b/c"
+value1 = key_values(object1, key1)
+print(value1)
+
+object2 = {"x": {"y": {"z": "a"}}}
+key2 = "x/y/z"
+value2 = key_values(object2, key2)
+print(value2)
